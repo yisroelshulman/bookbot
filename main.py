fa7ced5cdb992fdc85f358d140f890ed923a7485
yisroel <yisroelshulman16@gmail.com>
2024-01-09 11:06:27 -0500

    completed the book report

---
+++ main.py
@@ -0,0 +1,39 @@
+def main():
+    path = "books/frankenstein.txt"
+    text = read_contents(path)
+    num_words = word_count(text)
+    chars = char_count(text)
+    char_list = list(chars.items())
+    char_list.sort(key=lambda x: x[1], reverse=True)
+    print_report(path, num_words, char_list)
+
+def read_contents(path):
+    with open(path) as file:
+        return file.read()
+
+def word_count(file_contents):
+   return len(file_contents.split())
+
+def char_count(file_contents):
+    file_contents = file_contents.lower()
+    letters_dic = {}
+    for letter in file_contents:
+        if letter in letters_dic:
+            letters_dic[letter] += 1
+        else:
+            letters_dic[letter] = 1
+    return letters_dic
+
+def print_report(path, word_count, char_list):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in char_list:
+        if item[0].isalpha():
+            print(f"The '{item[0]}' character was found {item[1]} times")
+
+    print("--- End report ---")
+
+main()
+
